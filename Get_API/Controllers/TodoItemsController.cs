ce6e8f3f3f2068ce8733a13dc70ef70f8185da8c
unknown <redaattamahmoud@gmail.com>
2022-09-07 21:41:59 +0200

    first commit

---
+++ Get_API/Controllers/TodoItemsController.cs
@@ -0,0 +1,43 @@
+﻿using Get_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Get_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        List<TodoItem> todoitem = new List<TodoItem>();
+        public TodoItemsController()
+        {
+            todoitem.Add(new TodoItem
+            {
+                Id = 1,
+                Name="item1",
+                Iscomplete=true
+            });
+            todoitem.Add(new TodoItem
+            {
+                Id = 2,
+                Name = "item2",
+                Iscomplete = false
+            });
+            todoitem.Add(new TodoItem
+            {
+                Id = 3,
+                Name = "item3",
+                Iscomplete = true
+            });
+        }
+        [HttpGet]
+        public List<TodoItem> Get()
+        {
+            return todoitem;
+        }
+    }
+}
---
+++ Get_API/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Get_API.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Iscomplete { get; set; }
+    }
+}
